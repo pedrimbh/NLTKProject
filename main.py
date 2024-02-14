15998460568b7d1cd00f06a1c98c719225133114
Joao Pedro Mesquita <joaopmesquita92@gmail.com>
2024-02-14 20:53:07 -0300

    Update doc name and version

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from Routes.TokenizeRoute import router as token_router
 from Config.Config import Config
 
 Config()
-app = FastAPI(title="API NLTK Blip Tester", description="API que faz o uso da biblioteca NLTK", version="1.0.0")
+app = FastAPI(title="API NLTK Blip Teste", description="API que faz o uso da biblioteca NLTK", version="0.1.0")
 
 app.include_router(router=sentiment_analysis_router)
 app.include_router(router=token_router)
